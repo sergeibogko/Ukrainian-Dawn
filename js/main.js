8a72ab8136446456e05ee4a1204cb74e915ddd56
Sergei Bogko <69677370+sergeibogko@users.noreply.github.com>
2024-01-27 11:03:15 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,3 +4,35 @@ const menuMobile = document.querySelector('.header__list');
 menuBtn.addEventListener('click', () => {
   menuMobile.classList.toggle('menu--open');
 });
+
+const btnUp = {
+  el: document.querySelector('.arrow'),
+  show() {
+    // удалим у кнопки класс btn-up_hide
+    this.el.classList.remove('arrow__hide');
+  },
+  hide() {
+    // добавим к кнопке класс btn-up_hide
+    this.el.classList.add('arrow__hide');
+  },
+  addEventListener() {
+    // при прокрутке содержимого страницы
+    window.addEventListener('scroll', () => {
+      // определяем величину прокрутки
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+      scrollY > 400 ? this.show() : this.hide();
+    });
+    // при нажатии на кнопку .btn-up
+    document.querySelector('.arrow').onclick = () => {
+      // переместим в начало страницы
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    };
+  },
+};
+
+btnUp.addEventListener();
